11e311264bf49184714eec2e633021e3959c021d
anthony <anthony.ng@originbit.asia>
2026-09-16 16:49:52 +0800

    Create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'loader',
-    loadComponent: () => import('./pages/loader/loader.page').then( m => m.LoaderPage)
-  },
-  { path: '**', redirectTo: '/loader', pathMatch: 'full', },  {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
-
+  {
+    path: 'loader',
+    loadComponent: () => import('./pages/loader/loader.page').then( m => m.LoaderPage)
+  },
+  // Always place the wildcard route at the absolute bottom
+  { path: '**', redirectTo: '/loader', pathMatch: 'full', },
 ];
